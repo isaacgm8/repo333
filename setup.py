de467dbb5f5cd87798f9f2f2ea130adf788c20a3
isaacb <morenoisaac596@gmail.com>
2025-03-19 11:38:54 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repo333",                                 # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Isaac Bustamante",                         # Tu nombre
+    author_email="morenoisaac596@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/isaacgm8/repo333",     # URL del proyecto
+)
